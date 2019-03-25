01e4b886c9617ec23f8456047392a42cca30239b
Miguel Covarrubias <mcovarr@broadinstitute.org>
2019-03-25 11:26:02 -0400

    even less crap

--- src/ci/bin/testCentaurHoricromtal.sh
+++ src/ci/bin/testCentaurHoricromtal.sh
@@ -16,39 +16,13 @@ set -o errexit -o nounset -o pipefail
 
 cromwell::build::setup_common_environment
 
-if [[ "${CROMWELL_BUILD_PROVIDER}" == "${CROMWELL_BUILD_PROVIDER_TRAVIS}" ]]; then
-  # Upgrade docker-compose so that we get the correct exit codes
-  docker-compose -version
-  sudo rm /usr/local/bin/docker-compose
-  curl -L "https://github.com/docker/compose/releases/download/1.23.2/docker-compose-$(uname -s)-$(uname -m)" > docker-compose
-  chmod +x docker-compose
-  sudo mv docker-compose /usr/local/bin
-  docker-compose -version
-fi
-
 export TEST_CROMWELL_TAG=just-testing-horicromtal
 
 docker image ls -q broadinstitute/cromwell:"${TEST_CROMWELL_TAG}" | grep . || \
 CROMWELL_SBT_DOCKER_TAGS="${TEST_CROMWELL_TAG}" sbt server/docker
 
-#echo "ip addr show"
-#ip addr show || true
-
-
-#echo "Local IP command: ip addr show docker0 | grep 'inet ' | awk '{print \$2}' | cut -f1 -d'/'"
-#ip addr show docker0 | grep 'inet ' | awk '{print $2}' | cut -f1 -d'/' || true
-
-#export TEST_HOST_IP=$(ip addr show docker0 | grep 'inet ' | awk '{print $2}' | cut -f1 -d'/' || ipconfig getifaddr en0)
-
-# MYSQL_HOST_IP="${TEST_HOST_IP}" \
 CROMWELL_TAG="${TEST_CROMWELL_TAG}" \
 docker-compose -f scripts/docker-compose-mysql/docker-compose-horicromtal.yml up -d
-#docker-compose -f scripts/docker-compose-mysql/docker-compose-horicromtal.yml up --scale cromwell_soldier=2 -d
-
-# what the
-# Error: No such network: docker-compose-mysql_default
-#echo "docker network inspect docker-compose-mysql_default"
-#docker network inspect docker-compose-mysql_default
 
 # Give them some time to be ready
 sleep 30
@@ -58,6 +32,5 @@ CENTAUR_TEST_FILE=scripts/docker-compose-mysql/test/hello.test \
 sbt "centaur/it:testOnly *ExternalTestCaseSpec"
 
 # Tear everything down
-# MYSQL_HOST_IP="${TEST_HOST_IP}" \
 CROMWELL_TAG="${TEST_CROMWELL_TAG}" \
 docker-compose -f scripts/docker-compose-mysql/docker-compose-horicromtal.yml down
